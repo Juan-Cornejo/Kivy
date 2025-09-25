775e7db1006eaa1a26ed854ee26bf53a784214b5
sarita202422 <smarinao2024@alu.uct.cl>
2025-09-25 10:50:31 -0300

    intento de scrollview en proyectos

--- Portafolio/main.py
+++ Portafolio/main.py
@@ -1,6 +1,10 @@
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivymd.uix.card import MDCard
+from kivymd.uix.label import MDLabel
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import NumericProperty, StringProperty
 from kivymd.uix.button import MDFlatButton, MDIconButton
@@ -74,6 +78,43 @@ class ProyectosScreen(Screen):
 
     def volver_home(self):
         self.manager.current = "home"
+
+    def on_enter(self):
+        proyectos = [
+            {"titulo": "Proyecto 1", "descripcion": "Descripción 1", "imagen": "assets/img/luna.jpg", "link": "https://github.com/tuusuario/proyecto1"},
+            {"titulo": "Proyecto 2", "descripcion": "Descripción 2", "imagen": "assets/img/luna.jpg", "link": "https://github.com/tuusuario/proyecto2"},
+            {"titulo": "Proyecto 3", "descripcion": "Descripción 3", "imagen": "assets/img/luna.jpg", "link": "https://github.com/tuusuario/proyecto3"},
+             {"titulo": "Proyecto 4", "descripcion": "Descripción 1", "imagen": "assets/img/luna.jpg", "link": "https://github.com/tuusuario/proyecto1"},
+            {"titulo": "Proyecto 5", "descripcion": "Descripción 2", "imagen": "assets/img/luna.jpg", "link": "https://github.com/tuusuario/proyecto2"},
+            {"titulo": "Proyecto 6", "descripcion": "Descripción 3", "imagen": "assets/img/luna.jpg", "link": "https://github.com/tuusuario/proyecto3"}
+        ]
+
+        grid = self.ids.proyectos_grid
+        grid.clear_widgets()
+
+        for p in proyectos:
+            card = MDCard(size_hint=(None, None), size=("348dp", "170dp"), radius=[15], padding=0, md_bg_color=(225/255, 225/255, 225/255, 1), pos_hint={"center_x": 0.5})
+
+            box = MDBoxLayout(orientation="vertical", spacing=5, padding=0)
+
+            img = Image(source=p["imagen"], size_hint_y=None, height="80dp", allow_stretch=True, keep_ratio=False)
+
+            title = MDLabel(text=p["titulo"], font_style="H6", size_hint_y=None, halign="center")
+            title.height = title.texture_size[1]
+
+            description = MDLabel(text=p["descripcion"], theme_text_color="Secondary", size_hint_y=None, halign="center")
+            description.height = description.texture_size[1]
+            button = MDFlatButton(text="Ver más", pos_hint={"center_x": 0.5})
+
+
+            box.add_widget(img)
+            box.add_widget(title)
+            box.add_widget(description)
+            box.add_widget(button)
+
+            card.add_widget(box)
+
+            grid.add_widget(card)
 
 class SobreMiScreen(Screen):
     pass
